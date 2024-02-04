946f8f2f7d8d44700f25fa376414bc5e6be054ca
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-02-04 23:21:18 +0800

    Add files via upload
    ver1.04

--- ranger/commands.py
+++ ranger/commands.py
@@ -61,30 +61,6 @@ class my_edit(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
-class extract(Command):
-    """:extract <paths>
-    Extract archives
-    """
-    def execute(self):
-        import os
-        fail=[]
-        for i in self.fm.thistab.get_selection():
-            ExtractProg='7z x'
-            if i.path.endswith('.zip'):
-                # zip encoding issue
-                ExtractProg='unzip -O gbk'
-            elif i.path.endswith('.tar.gz'):
-                ExtractProg='tar xvf'
-            elif i.path.endswith('.tar.xz'):
-                ExtractProg='tar xJvf'
-            elif i.path.endswith('.tar.bz2'):
-                ExtractProg='tar xjvf'
-            if os.system('{0} "{1}"'.format(ExtractProg, i.path)):
-                fail.append(i.path)
-        if len(fail) > 0:
-            self.fm.notify("Fail to extract: {0}".format(' '.join(fail)), duration=10, bad=True)
-        self.fm.redraw_window()
-        
 
 import os
 from ranger.core.loader import CommandLoader
@@ -108,13 +84,48 @@ class compress(Command):
 
         descr = "compressing files in: " + os.path.basename(parts[1])
         obj = CommandLoader(args=['apack'] + au_flags + \
-                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
 
-    def tab(self):
+    def tab(self, tabnum):
         """ Complete with current folder name """
 
         extension = ['.zip', '.tar.gz', '.rar', '.7z']
         return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+
+
+import os
+from ranger.core.loader import CommandLoader
+
+class extract(Command):
+    def execute(self):
+        """ extract selected files to current directory."""
+        cwd = self.fm.thisdir
+        marked_files = tuple(cwd.get_selection())
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = marked_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-x', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(marked_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags
+                            + [f.path for f in marked_files], descr=descr,
+                            read=True)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
