10b01064f9ba9dfeb0edecdc8bd72969b0724007
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-02-05 18:33:01 +0800

    Update switcher.sh

--- waybar/scripts/switcher.sh
+++ waybar/scripts/switcher.sh
@@ -32,7 +32,5 @@ for ((i=0; i<num_files; i++)); do
 		else
 			swww img "${files[i+1]}" --transition-fps 80 --transition-type grow --transition-pos top-right --transition-duration 1.3 --transition-bezier 0.43,1.19,1,0.4
 		fi
-	else
-		notify-send -u critical "Failed!" "you should init swww first"
 	fi
 done
